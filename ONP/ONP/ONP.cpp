39d7472e6b6e1bd9147615917b664a35305b02d2
Pacto <pact0@users.noreply.github.com>
2020-02-26 23:41:11 +0100

    ONP with () working
    ONP with () working

--- ONP/ONP/ONP.cpp
+++ ONP/ONP/ONP.cpp
@@ -41,27 +41,31 @@ int isDigit (char c)
 {
 	return(c >= '0' && c <= '9');
 }
-int isOper (char nOper) // + - / ^ *
+int isOper (char nOper) // () + - / ^ *
 {
 	switch(nOper)
 	{
 	case '+': //move to next
 	case '-': //++
+	case '(': //++
+	case ')': //++
 	case '/': //++
 	case '*': //++
 	case '^': return 1;
 	}
 	return 0;
 }
-int prior (char nOper)// priority of oper    1. (+ -)  2. (* /) 3. ^
+int prior (char nOper)// priority of oper     1.(+ -)  2. (* /) 3. ^
 {
 	switch (nOper)
 	{
-	case '+': 
-	case '-': return 1;
+	case '(': return 1;
+	case ')': return 2;
+	case '+':
+	case '-': return 3;
 	case '/': 
-	case '*': return 2;
-	case '^': return 3;
+	case '*': return 4;
+	case '^': return 5;
 	}
 	return 0;
 }
@@ -70,15 +74,38 @@ char getOper()
 	skipSpaces();
 	return getchar();
 }
-void onp ()
+void onp ()// (1+1) * ( 1+2 )
 {
 	char c;
+	char d;
+	while( ( c = getOper() ) == '(' )
+		push(c);
+	ungetc(c, stdin);
+
 	printf("%d", getNum());					//assumed that always a number 1sr
 	while( isOper(c = getOper()) )			//always an operand later
 	{
-		while( prior(c) <= prior(top()) )	
-			printf("%c", pop());			//print and delete all oper 
+		
+
+		while( prior(c) <= prior(top()) )
+		{
+			if( top() == '(' )
+				break;
+			printf("%c", pop());			//print and delete all oper 	
+		}
+		if( c == ')' )
+
+			{
+				while( top() != '(' )
+					printf("%c", pop());
+				del();
+				continue;					
+			}
 		push(c);							//push c to top
+		
+		while( ( c = getOper() ) == '(' )
+			push(c);
+		ungetc(c, stdin);
 		printf("%d", getNum());				//always a number after operands
 	}
 
